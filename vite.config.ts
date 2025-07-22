2e39ef64f70e34875075db40bbab3595a95301a4
Sengiria <sengiria.wymer@gmail.com>
2025-07-23 01:03:25 +0200

    fix incorrect base name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/artScope/',
+    base: '/art-scope-frontend/',
   plugins: [
     react(),
     tailwindcss()
